f02686b495319652b182570380732d4ad369facc
iruzts <ruizsoto1996@gmail.com>
2021-03-27 18:58:51 -0600

    controladores

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UnoController;
+use App\Http\Controllers\DosController;
+use App\Http\Controllers\TresController;
+use App\Http\Controllers\CuatroController;
+use App\Http\Controllers\CincoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,22 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/ruta1', function () {
-    return ('Ruta 1');
-});
-
-Route::get('/ruta2', function () {
-    return ('Ruta 2');
-});
-
-Route::get('/ruta3', function () {
-    return ('Ruta 3');
-});
-
-Route::get('/ruta4', function () {
-    return ('Ruta 4');
-});
-
-Route::get('/ruta5', function () {
-    return ('Ruta 5');
-});
+//Route::get('/ruta1', function () {
+  //  return ('Ruta 1');
+//});
+Route::get('/ruta1/{id}', [UnoController::class, 'show']);
+Route::get('/ruta2/{id}', [DosController::class, 'show']);
+Route::get('/ruta3/{id}', [TresController::class, 'show']);
+Route::get('/ruta4/{id}', [CuatroController::class, 'show']);
+Route::get('/ruta5/{id}', [CincoController::class, 'show']);
